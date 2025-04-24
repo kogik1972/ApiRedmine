f51fe5979737ca7c85616a34f858706c5119c6c0
Juan Pablo Alvarez <EXIT>
2025-04-23 22:26:57 -0400

    feat: registrar issue_id en la tabla documentos

--- db/db_models.py
+++ db/db_models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from app import db
+import pytz
+tz_cl = pytz.timezone('America/Santiago')
 
 class Documento(db.Model):
     __tablename__ = 'documentos'
@@ -7,8 +9,9 @@ class Documento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     path_pdf = db.Column(db.String(512), nullable=False)
-    fecha_generacion = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha_generacion = db.Column(db.DateTime, default=lambda: datetime.now(tz_cl))
     estado_firma = db.Column(db.String(20), default='pendiente')  # pendiente, firmado, rechazado
+    issue_id = db.Column(db.Integer, nullable=True, index=True)
 
     firmas = db.relationship('FirmaRequerida', backref='documento', lazy=True)
 
