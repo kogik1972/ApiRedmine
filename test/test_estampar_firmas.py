417148733075f237713fd204504e190aafe2e09a
Juan Pablo Alvarez <EXIT>
2025-04-26 02:21:55 -0400

    chore: mejorar test_estampar_firmas.py para detección dinámica de sistema operativo

--- test/test_estampar_firmas.py
+++ test/test_estampar_firmas.py
@@ -6,19 +6,29 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
+import platform
 import logging
 from firma.estampar_firmas import estampar_firmas
 
-# Logging básico
+# Configurar logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def main():
     issue_id = 1234
     nombre_documento = "4.477.432-1_20250424_205215_AnexoCambioSueldo.docx"
 
-    # RUTA PORTABLE
-    base_dir = os.path.join("/","redmine-5.1.3", "files", "2025", "04")
-    path_documento = os.path.abspath(base_dir)
+    # Detectar sistema operativo
+    sistema = platform.system()
+
+    if sistema == "Windows":
+        base_dir = os.path.join("D:\\", "redmine-5.1.3", "files", "2025", "04")
+    elif sistema == "Linux":
+        base_dir = os.path.join("/home", "desa", "Data", "redmine-5.1.3", "files", "2025", "04")
+    else:
+        logging.error(f"Sistema operativo no soportado: {sistema}")
+        return
+
+    path_documento = base_dir
 
     firmas_requeridas = [
         {"nombre": "Juan Pablo Alvarez", "rut": "12.262.608-3", "fecha_firma": "25-04-2025 01:25", "tipo": "firmante"},
