daefc66299f9f3482d083733766fe70d665acdec
Juan Pablo Alvarez <EXIT>
2025-05-01 19:28:42 -0400

    feat: agrega manejo de logger unico 1.3

--- utils/convertidor.py
+++ utils/convertidor.py
@@ -11,24 +11,31 @@ def convierto_docx2pdf(nombre_documento, path_documento):
     docx_path = os.path.join(path_documento, nombre_documento)
 
     if not os.path.isfile(docx_path):
-        logger.warning(f"Archivo no encontrado: {docx_path}")
+        logger.info(f"Archivo no encontrado: {docx_path}")
         return None
+
+    # Directorio de salida (el mismo que el del archivo DOCX)
+    output_dir = os.path.dirname(docx_path)
+
+    # Comando para convertir el archivo usando LibreOffice
+    command = ['/usr/bin/soffice', '--headless', '--convert-to', 'pdf', docx_path, '--outdir', output_dir]
 
     try:
         # Usa LibreOffice para convertir el DOCX a PDF
-        subprocess.run([
-            "libreoffice",
-            "--headless",
-            "--convert-to", "pdf",
-            "--outdir", path_documento,
-            docx_path
-        ], check=True)
+        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        logger.info(f"Stdout: {result.stdout.decode()}")
+        logger.info(f"Stderr: {result.stderr.decode()}")
 
         nombre_sin_extension = os.path.splitext(nombre_documento)[0]
         nombre_documento_pdf = f"{nombre_sin_extension}.pdf"
-        logger.info(f"Archivo convertido exitosamente: {nombre_documento_pdf}")
-        return nombre_documento_pdf
+        return os.path.join(output_dir, nombre_documento_pdf)
+
 
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error al convertir el documento con LibreOffice: {e}", exc_info=True)
-        return None
+        logger.critical(f'Ocurrió un error durante la conversión: {e.stderr.decode()}')
+        return 500
+    except Exception as e:
+        logger.critical(f'Ocurrió un error: {e}')
+        return 500
+
