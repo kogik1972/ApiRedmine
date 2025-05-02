2ea32fef8c4c90e66bf84d7e521b103af594ae56
Juan Pablo Alvarez <EXIT>
2025-05-01 20:38:12 -0400

    feat: agrega conversion docx a pdf - saco timestamp 2.3

--- scripts/archivo_manager.py
+++ scripts/archivo_manager.py
@@ -12,12 +12,13 @@ logger = logging.getLogger(__name__)
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 def mover_a_docs(origen, nombre_archivo):
+    nuevo_nombre = nombre_archivo
     destino_dir = os.path.join(ROOT_DIR, 'docs')  # 👈 ahora apunta a /docs directamente
     os.makedirs(destino_dir, exist_ok=True)
 
-    base, ext = os.path.splitext(nombre_archivo)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    nuevo_nombre = f"{base}_{timestamp}{ext}"
+    #base, ext = os.path.splitext(nombre_archivo)
+    #timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    #nuevo_nombre = f"{base}_{timestamp}{ext}"
     destino = os.path.join(destino_dir, nuevo_nombre)
 
     shutil.copy2(origen, destino)
